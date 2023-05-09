3aab5862ab5b40f7b12aef8c00ddd023bd034c32
Marcos BiG <marcosdlopes21@gmail.com>
2023-05-09 18:26:51 -0300

    Atv10

---
+++ ATV_JS/Questão9/Js9.js
@@ -0,0 +1,12 @@
+function printArray() {
+    var input = document.getElementById("input").value;
+    var array = input.split(",");
+    var output = document.getElementById("output");
+    output.innerHTML = "";
+    for (var i = 0; i < array.length; i++) {
+      var item = document.createElement("div");
+      item.className = "item";
+      item.textContent = array[i].trim();
+      output.appendChild(item);
+    }
+  }
