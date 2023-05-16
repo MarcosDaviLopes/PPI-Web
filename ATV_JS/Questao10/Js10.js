3daa9cd971e7d33694de3736f91322b40597688a
Macos BiG <marcosdlopes21@gmail.com>
2023-05-16 09:06:19 -0300

    alter

--- ATV_JS/Questao10/Js10.js
+++ ATV_JS/Questao10/Js10.js
@@ -1,31 +1,48 @@
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
+var operador1 = "";
+var operador2 = "";
+var operacao = "";
 
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-   
-    const buttonValue = button.textContent;
-
-    
-    switch (buttonValue) {
-      case 'C':
-       
-        display.value = '';
-        break;
-      case '=':
-        
-        try {
-          const result = eval(display.value);
-          display.value = result;
-        } catch (e) {
-          display.value = 'Erro';
-        }
-        break;
-      default:
-        
-        display.value += buttonValue;
-        break;
+function inserirNumero(numero) {
+    if (operacao === "") {
+        operador1 += numero;
+        document.getElementById("resultado").value = operador1;
+    } else {
+        operador2 += numero;
+        document.getElementById("resultado").value = operador2;
     }
-  });
-});
+}
+
+function definirOperacao(op) {
+    if (operador1 !== "") {
+        operacao = op;
+        document.getElementById("resultado").value += " " + operacao + " ";
+    }
+}
+
+function calcular() {
+    var resultado;
+
+    switch (operacao) {
+        case "+":
+            resultado = parseFloat(operador1) + parseFloat(operador2);
+            break;
+        case "-":
+            resultado = parseFloat(operador1) - parseFloat(operador2);
+            break;
+        case "*":
+            resultado = parseFloat(operador1) * parseFloat(operador2);
+            break;
+        case "/":
+            resultado = parseFloat(operador1) / parseFloat(operador2);
+            break;
+        default:
+            alert("Operação inválida!");
+            return;
+    }
+    function limpar() {
+      operador1 = "";
+      operador2 = "";
+      operacao = "";
+      document.getElementById("resultado").value = "";
+      }
+}
--- ATV_JS/Questão9/Js9.js
+++ ATV_JS/Questão9/Js9.js
@@ -2,7 +2,7 @@ function printArray() {
     var input = document.getElementById("input").value;
     var array = input.split(",");
     var output = document.getElementById("output");
-    output.innerHTML = "";
+    input.innerHTML = "";
     for (var i = 0; i < array.length; i++) {
       var item = document.createElement("div");
       item.className = "item";
